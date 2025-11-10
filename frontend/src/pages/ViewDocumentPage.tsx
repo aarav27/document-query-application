fe20fc2a3df54c629383607a95def9aaf7cda548
aarav27 <aaravsharma927@gmail.com>
2025-11-10 15:29:17 -0800

    Added document error and loading handling

--- frontend/src/pages/ViewDocumentPage.tsx
+++ frontend/src/pages/ViewDocumentPage.tsx
@@ -8,19 +8,32 @@ export default function ViewDocumentPage() {
   const document : DocumentType = state.document;
   const category : string = state.category;
   const [downloadURL, setDownloadURL] = useState<string | null>(null)
+  const [documentLoading, setDocumentLoading] = useState<boolean>(true)
+  const [documentError, setDocumentError] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchDocument = async() => {
-      const download_url_response = await fetch(
-        `http://127.0.0.1:8000/download-url/${encodeURIComponent(document.s3_document_key)}?document_name=${encodeURIComponent(document.name)}`)
-      if (!download_url_response.ok){
-        throw new Error(`Error Status: ${download_url_response.status}`);
+      try {
+        if (document.s3_document_key){
+          const download_url_response = await fetch(
+            `http://127.0.0.1:8000/download-url/${encodeURIComponent(document.s3_document_key)}?document_name=${encodeURIComponent(document.name)}`)
+          if (!download_url_response.ok){
+            throw new Error(`Error Status: ${download_url_response.status}`);
+          }
+          const { download_url } = await download_url_response.json()
+          setDownloadURL(download_url)
+        }
+        else{
+          setDocumentError(true)
+        }
+      } catch {
+        setDocumentError(true)
+      } finally {
+        setDocumentLoading(false)
       }
-      const { download_url } = await download_url_response.json()
-      setDownloadURL(download_url)
     };
     fetchDocument()
-  }, [document])
+  }, [document, documentError, documentLoading])
 
   return (
     <div className="view-document-page">
@@ -28,15 +41,19 @@ export default function ViewDocumentPage() {
       <h2 className="document-category">{category}</h2> 
       <p className="document-description">{document.description}</p> 
       <div className="document-file">
-        {downloadURL ? (
+        {documentLoading ? (
+          <></>
+        ) : documentError ? (
+          <div className="error-text">
+            Document Not Found
+          </div>
+        ) : downloadURL ? (
           <iframe
             src={downloadURL}
-            className = "document-iframe"
+            className="document-iframe"
+            title="PDF Document"
           />
-        ) : 
-          <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-            Loading Document...
-          </div>}
+        ) : null}
       </div>
     </div>
   )
