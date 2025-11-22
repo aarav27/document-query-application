3c2b3acb12a4bb881ec940ac202aea91e8de101b
aarav27 <aaravsharma927@gmail.com>
2025-11-21 21:53:46 -0800

    Added functionality for search

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import { useDocumentsAndCategories } from '@/components/FetchData'
+import type { DocumentType } from '@/util/types';
 import '@/styles/home.css'
 import '@/styles/search.css'
 
@@ -7,61 +8,84 @@ export default function SearchPage(){
     const { 
         loading,
         error,
-        categoryMap,
-        // categoryDocumentMap,
+        categoryDocumentMap,
     } = useDocumentsAndCategories();
     const [selectedCategory, setSelectedCategory] = useState("All");
     const [searchInput, setSearchInput] = useState("");
-    // const [documentsSearchResult, setDocumentsSearchResult] = useState("");
+    const [documentsSearchResult, setDocumentsSearchResult] = useState<DocumentType[] | null>(null)
 
     const handleSearch = () => {
+        // 0. Check if text in search input
+        if (searchInput === ""){
+            return;
+        }
+        alert(searchInput)
 
-        if (selectedCategory)
-        alert("Searched for something");
+        // 1. Select documents based on selected category
+        let filteredDocuments : DocumentType[] = []
+        if (selectedCategory === "All"){
+            Object.keys(categoryDocumentMap).forEach((category : string) => {
+                filteredDocuments.push(...categoryDocumentMap[category])
+            })
+        }
+        else if(categoryDocumentMap[selectedCategory]){
+            filteredDocuments = categoryDocumentMap[selectedCategory]
+        }
+        else{
+            alert("Invalid category")
+            return;
+        }
+
+        // 2. Perform search
+        const search_result : DocumentType[] = filteredDocuments.filter((document : DocumentType) => 
+            document.extracted_text && document.extracted_text.toLowerCase().includes(searchInput.toLowerCase())
+        )
+        setDocumentsSearchResult(search_result);
+        alert("Searched for " + searchInput);
     }
 
-    if (loading) return <div></div>;
+    if (loading) return <div/>;
     if (error) return <div>Error loading documents</div>;
 
-    return (
-        
-    <div>
-        <div className='search-top'>
-            {/* Title */}
-            <h1 className='page-title'>Search</h1>
-        </div>
-
-        <div className="search-controls">
-            {/* Category Filters*/}
-            <div className="category-filter">
-                <select
-                    id="category"
-                    className="category-dropdown"
-                    value={selectedCategory}
-                    onChange={(e) => setSelectedCategory(e.target.value)}
-                >
-                    <option value="All">All Categories</option>
-                    {Object.keys(categoryMap).map((cat) => (
-                        <option key={cat} value={cat}>
-                            {cat}
-                        </option>
-                    ))}
-                </select>
+    return (    
+        <div>
+            <div className='search-top'>
+                {/* Title */}
+                <h1 className='page-title'>Document Search</h1>
             </div>
 
-            {/* Search Bar*/}
-            <div className="search-bar">
-                <input
-                    type="search"
-                    placeholder='Search Here'
-                    onChange={(e) => setSearchInput(e.target.value)}
-                    onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-                    value={searchInput}
-                />
-                <button className="search-button" onClick={handleSearch}>
-                    🔍 Search
-                </button>
+            <div className="search-controls">
+                {/* Category Filters*/}
+                <div className="category-filter">
+                    <select
+                        id="category"
+                        className="category-dropdown"
+                        value={selectedCategory}
+                        onChange={(e) => setSelectedCategory(e.target.value)}
+                    >
+                        <option value="All">All Categories</option>
+                        {Object.keys(categoryDocumentMap).map((cat) => (
+                            <option key={cat} value={cat}>
+                                {cat}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                {/* Search Bar*/}
+                <div className="search-bar">
+                    <input
+                        type="search"
+                        placeholder='Search Here'
+                        onChange={(e) => setSearchInput(e.target.value)}
+                        onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+                        value={searchInput}
+                    />
+                    <button className="search-button" onClick={handleSearch}>
+                        Search&nbsp; 🔍 
+                    </button>
+                </div>
             </div>
         </div>
-    </div>)
+    )
 }
