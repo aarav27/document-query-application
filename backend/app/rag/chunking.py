c2299f10feab295b7f6ada6c89049a086121c58f
aarav27 <aaravsharma927@gmail.com>
2026-02-01 15:50:36 -0800

    RAG/search clean up

--- backend/app/rag/chunking.py
+++
@@ -1,9 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def character_chunking(documents):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=150,
-        separators=["\n\n", "\n", " ", ""]
-    )
-    return splitter.split_documents(documents)
--- backend/app/rag/vector_db.py
+++
@@ -1,3 +0,0 @@
-from langchain_community.vectorstores import Chroma
-
-vectordb = Chroma(persist_directory="./chroma_db")
