7547426ca785e913a7e25ef0d4cc62aac0b3670a
aarav27 <aaravsharma927@gmail.com>
2026-02-21 22:18:11 -0800

    Updated vector store and llm to use

--- backend/app/rag/models.py
+++ backend/app/rag/models.py
@@ -2,22 +2,27 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2Se
 import torch
 
 PHI_3_MINI = "microsoft/Phi-3-mini-4k-instruct"
-FLAN_T5_MODEL = "google/flan-t5-small" 
+FLAN_T5_MODEL = "google/flan-t5-large" 
+QWEN2 = "Qwen/Qwen2-1.5B-Instruct"
 
-def get_flan_llm():
-    model_name: str = FLAN_T5_MODEL
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(
-        model_name,
-        
-        dtype=torch.float32,
-        device_map="auto"
-    )
-    pipe = pipeline(
-        "text2text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_new_tokens=300,
-        temperature=0.2
-    )
-    return pipe
+_llm_pipeline = None
+
+def get_llm(model_str: str = QWEN2):
+    global _llm_pipeline
+    if _llm_pipeline is None:
+        tokenizer = AutoTokenizer.from_pretrained(model_str)
+        model = AutoModelForCausalLM.from_pretrained(
+            model_str,
+            dtype=torch.float16,
+            device_map="auto"
+        )
+        _llm_pipeline = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=300,
+            temperature=0.2,
+            do_sample=True,
+            return_full_text=False
+        )
+    return _llm_pipeline
--- backend/app/rag/vector_stores.py
+++ backend/app/rag/vector_stores.py
@@ -1,6 +1,8 @@
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from app.rag.embeddings import get_sentence_transformers
+
 chroma_db = Chroma(
+    collection_name="DocQuery_ChromaDB",
     persist_directory="./chroma_db",
-    embedding_function=get_sentence_transformers()
+    embedding_function=get_sentence_transformers(),
 )
