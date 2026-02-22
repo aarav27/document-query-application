83ae402c7069e0e1c8b3bbbc8f6e6272aa691ea3
aarav27 <aaravsharma927@gmail.com>
2026-02-22 00:02:23 -0800

    Use cosine dist, change from L2 default

--- backend/app/rag/vector_stores.py
+++ backend/app/rag/vector_stores.py
@@ -5,4 +5,5 @@ chroma_db = Chroma(
     collection_name="DocQuery_ChromaDB",
     persist_directory="./chroma_db",
     embedding_function=get_sentence_transformers(),
+    collection_metadata={"hnsw:space": "cosine"}
 )
